b2b7def0d1f253ffad1622f5fb87bab43d1e6703
amar <gona>
2022-07-17 22:56:29 +0530

    init commit

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,8 @@
 package batch
 
 import (
+	"fmt"
+	"sync"
 	"time"
 )
 
@@ -14,5 +16,53 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+
+	steps, addOn := calculateStartAndEnd(n, pool)
+
+	var wg sync.WaitGroup
+
+	ch := make(chan user, n+5)
+
+	data := make([]user, 0)
+
+	for i := int64(1); i <= pool; i++ {
+
+		wg.Add(int(i))
+		start := (i * steps) - steps
+		end := i * steps
+
+		if i == steps && addOn > 0 {
+			end = i*steps + addOn
+		}
+
+		go getBatchUsers(start, end, ch, &wg)
+
+	}
+
+	wg.Wait()
+	fmt.Println(len(ch))
+	for v := range ch {
+		fmt.Println(v)
+		data = append(data, v)
+	}
+
+	return data
+}
+
+func getBatchUsers(start int64, end int64, ch chan user, wg *sync.WaitGroup) {
+
+	defer wg.Done()
+	for i := start; i < end; i++ {
+		user := getOne(i)
+		ch <- user
+	}
+
+}
+
+func calculateStartAndEnd(n int64, pool int64) (steps int64, addOn int64) {
+
+	steps = n / pool
+	addOn = n % pool
+
+	return steps, addOn
 }
